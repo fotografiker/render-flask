a7ca5c02d8271c2bf60db3d105a1768277833726
fotografiker <cagrioner@aof.anadolu.edu.tr>
2023-05-22 16:05:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
+cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000/python"}})
 
 @app.route('/')
 def hello_world():
