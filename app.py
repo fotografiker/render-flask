1ee3bbb7810f682bced7184124cf4a3ad74815a4
fotografiker <cagrioner@aof.anadolu.edu.tr>
2023-05-22 15:42:03 +0300

    app.py sample api endpoint

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    message = {}
+    data = {}
+    
+    message['message'] = 'Hello World from Flask!'
+    data['status'] = 200
+    data['data'] = message
+    
+    return jsonify(data)
+if __name__ == '__main__':
+    app.run(debug=True)
